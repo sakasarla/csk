692f9cd70e1bff1244aa84a79ebe1a79586d0fa6
sakasarla <samratkasarla26@gmail.com>
2026-01-18 02:14:37 +0530

    Update git.add.commit.push.sh

--- git.add.commit.push.sh
+++ git.add.commit.push.sh
@@ -7,7 +7,7 @@ git add .
 git commit -m "changes"
 
 
-git pull https://sakasarla:@github.com/sakasarla/python.git
+git pull https://sakasarla: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@github.com/sakasarla/csk.git
 
 
-git push https://sakasarla:@github.com/sakasarla/python.git
+git push https://sakasarla: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@github.com/sakasarla/csk.git
